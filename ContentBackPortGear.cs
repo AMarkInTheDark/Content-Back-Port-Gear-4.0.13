edb7b1fa727a510dbd8f88c0fe937ddd8d315110
AMarkInTheDark <127246522+AMarkInTheDark@users.noreply.github.com>
2026-05-09 23:18:16 -0600

    Add files via upload

---
+++ ContentBackPortGear.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+namespace ContentBackPortGear;
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+public class ContentBackPortGear(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon
+) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+        await Task.CompletedTask;
+    }
+}
